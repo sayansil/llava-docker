29493ea2a7a75cfb10e52ebe9f100479deb84a42
Sayan Sil <sayan227sil@ymail.com>
2024-07-14 22:47:42 +0530

    Update start_controller.sh

--- scripts/start_controller.sh
+++ scripts/start_controller.sh
@@ -5,5 +5,6 @@ source /venv/bin/activate
 cd /workspace/LLaVA
 nohup python3 -m llava.serve.controller \
   --host ${LLAVA_HOST} \
+  --share \
   --port ${LLAVA_CONTROLLER_PORT} > /workspace/logs/controller.log 2>&1 &
 deactivate
