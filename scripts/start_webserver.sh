f81a73b147554c127509b8646b0527bc54cf38e0
Sayan Sil <sayan227sil@ymail.com>
2024-07-14 22:47:19 +0530

    Update start_webserver.sh

--- scripts/start_webserver.sh
+++ scripts/start_webserver.sh
@@ -5,5 +5,6 @@ source /venv/bin/activate
 cd /workspace/LLaVA
 nohup python -m llava.serve.gradio_web_server \
   --controller http://localhost:${LLAVA_CONTROLLER_PORT} \
+  --share \
   --model-list-mode reload > /workspace/logs/webserver.log 2>&1 &
 deactivate
