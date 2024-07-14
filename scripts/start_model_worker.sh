2a429ae175a51865305797490093ff45510c4a31
Sayan Sil <sayan227sil@ymail.com>
2024-07-14 22:48:10 +0530

    Update start_model_worker.sh

--- scripts/start_model_worker.sh
+++ scripts/start_model_worker.sh
@@ -6,6 +6,7 @@ cd /workspace/LLaVA
 nohup python3 -m llava.serve.model_worker \
   --host ${LLAVA_HOST} \
   --controller http://localhost:${LLAVA_CONTROLLER_PORT} \
+  --share \
   --port ${LLAVA_MODEL_WORKER_PORT} \
   --worker http://localhost:${LLAVA_MODEL_WORKER_PORT} \
   --model-path ${LLAVA_MODEL} > /workspace/logs/model-worker.log 2>&1 &
